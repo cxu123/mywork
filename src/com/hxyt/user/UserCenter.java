34a18c74642573904aca78ad0e311c673710a7ec
luck <496355474@qq.com>
2015-10-14 17:48:15 +0800

    添加用户中心

---
+++ src/com/hxyt/user/UserCenter.java
@@ -0,0 +1,29 @@
+package com.hxyt.user;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/** 
+ * @author  作者 陈修园
+ * @date 创建时间：2015-10-14 下午5:45:41 
+ * @version 1.0 
+ * @parameter  
+ * @since  
+ * @return  
+ */
+public class UserCenter extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	
+}
